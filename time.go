ec2c0ba63df9550b7ee1c63384ee770b63753751
andrew <andry925@ukr.net>
2024-02-14 20:03:21 +0200

    Adding functionality to return current time

--- time.go
+++ time.go
@@ -1,13 +1,28 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func serverHandler(writer http.ResponseWriter, response *http.Request) {
 	writer.Header().Set("Content-Type", "application/json")
-	writer.Write([]byte("time"))
+	currentTime := getCurrentTime()
+	resp := map[string]string{"currentTime": currentTime}
+	ResponseInJson, err := json.Marshal(resp)
+	if err != nil {
+		http.Error(writer, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	writer.Write(ResponseInJson)
+}
+
+func getCurrentTime() string {
+	currentTime := time.Now().Format(time.RFC3339)
+	return currentTime
 }
 
 func main() {
