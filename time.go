476b8fa9c1f9105927e5a230f22186b885a27168
Igor <114025058+igaryakqwe@users.noreply.github.com>
2024-02-14 18:22:36 +0200

    added http server

--- time.go
+++ time.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
+func serverHandler(writer http.ResponseWriter, response *http.Request) {
+	writer.Header().Set("Content-Type", "application/json")
+	writer.Write([]byte("time"))
+}
+
 func main() {
-	println("hello world")
+	http.HandleFunc("/time", serverHandler)
+
+	port := 8000
+	println("Server is running on port:", fmt.Sprint(port))
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	if err != nil {
+		fmt.Println("Server error:", err)
+	}
 }
